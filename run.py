8606115117716e03378aac80b2a832a663dea659
Sasikanth Kotti <kotti.sasikanth@gmail.com>
2021-12-25 18:52:24 +0530

    Update run.py

--- run.py
+++ run.py
@@ -59,6 +59,7 @@ model = PULSE(cache_dir=kwargs["cache_dir"])
 model = DataParallel(model)
 
 toPIL = torchvision.transforms.ToPILImage()
+print("*********"
 
 for ref_im, ref_im_name in dataloader:
     if(kwargs["save_intermediate"]):
